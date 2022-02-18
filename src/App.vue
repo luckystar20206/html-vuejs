83b5a06c5c41f7e5453d7787a8c9c102dd451478
luckystar20206 <luckystar20206@gmail.com>
2022-02-18 11:47:44 +0100

    added variables scss, bootstrap and axios

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
 <template>
-  <div id="app">
-
-  </div>
+	<div id="app"></div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  components: {
-
-  }
-}
+	name: "App",
+	components: {},
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-}
+@import "./assets/sass/style.scss";
 </style>
